2856c6e2c36167de78b0774cc879c4f315628d70
George <georgejoj28@gmail.com>
2024-01-08 14:34:11 +0300

    add templates

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from flask import url_for
+from flask import render_template
+
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('index.html')
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+@app.route('/user/<username>')
+def profile(username):
+    return f'{username}\'s profile'
+
+@app.route('/login')
+def login():
+    return 'login'
+
+with app.test_request_context():
+    print(url_for('home'))
+    print(url_for('about'))
+    print(url_for('login'))
+    print(url_for('profile', username='John Doe'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
